3f35db1f6a7f24e0efa7f7d9628a699db9b58239
virensareen <124622353+virensareen@users.noreply.github.com>
2023-08-04 17:11:12 -0700

    Uploading project

---
+++ Parenthesis Validator/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Parenthesis_Validator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 25;
+            this.listBox.Location = new System.Drawing.Point(90, 60);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(560, 329);
+            this.listBox.TabIndex = 0;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
+
---
+++ Parenthesis Validator/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Parenthesis_Validator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ValidateParentheses();
+        }
+
+        private void ValidateParentheses()
+        {
+            var filePath = "parenthesis_data.csv";
+
+            if (File.Exists(filePath))
+            {
+                var lines = File.ReadAllLines(filePath);
+
+                foreach (string line in lines)
+                {
+                    if (IsValid(line))
+                    {
+                        listBox.Items.Add(line);
+                    }
+                }
+            }
+        }
+
+        private bool IsValid(string input)
+        {
+            int openCount = 0;
+
+            foreach (char c in input)
+            {
+                if (c == '(')
+                {
+                    openCount++;
+                }
+                else if (c == ')')
+                {
+                    openCount--;
+
+                    if (openCount < 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return openCount == 0;
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
